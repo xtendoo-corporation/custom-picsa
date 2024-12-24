f16842bbed38568512944805d73e4f453b5f2ea6
Gokac9 <theabraham9@gmail.com>
2024-12-24 07:57:21 +0100

    output budget/order report units

---
+++ picsa_customization/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Picsa Customization",
+    "version": "17.0.1.0.1",
+    "category": "Customization",
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/report_saleorder_document.xml",
+    ],
+    "installable": True,
+}
